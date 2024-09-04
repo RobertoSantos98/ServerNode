3d5bede739841f2471ed6f5362c59b3c7f45c002
Roberto <roberto.felipe467@gmail.com>
2024-09-04 20:56:04 -0300

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+const app = express();
+
+const SECRET_KEY = 'Sua_JWT_chave_secreta';
+
+app.use(express.json());
+
+const gerarToken = ( user ) => {
+        return jwt.sign({ id:user.id, email: user.email }, SECRET_KEY, { expiresIn: '1h'});
+}
+
+app.post('/register', async ( req, res ) => {
+
+    const { name, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash( password, 10);
+        await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }
+    })
+        console.log("Um novo usuario foi adicionado no banco de dados.")
+        res.status(201).json(req.body)
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try{
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email,
+            }
+        })
+        if (!user) {
+            res.status(404).json("Usuário não encontrado.");
+            throw new Error('Usuário não encontrado."');
+        }
+
+        if(await bcrypt.compare(password, user.password)){
+            const token = gerarToken(user)
+            res.json({
+                name: user.name,
+                token: token
+            });
+            
+
+            console.log("Um usuário acabou de Logar.")
+        } else {
+            res.status(401).json("Credenciais Inválidas.");
+        }
+
+    }catch (error){
+        res.status(500).json({ error: error.message});
+    }
+
+
+})
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3333;
+const host = '0.0.0.0';
+
+
+app.listen(port, host, () => {
+    console.log( "Servidor está rodando...")
+})
+
