d16b931f8fde27e4aaa39f5c83e672978e5d66bc
Roberto <roberto.felipe467@gmail.com>
2024-09-04 21:09:54 -0300

    aaaa

---
+++ build.js
@@ -0,0 +1,9 @@
+const { execSync } = require('child_process');
+
+const environment = process.env.NODE_ENV || 'development';
+
+if (environment === 'production') {
+  execSync('webpack --config webpack.prod.js', { stdio: 'inherit' });
+} else {
+  execSync('webpack --config webpack.dev.js', { stdio: 'inherit' });
+}
